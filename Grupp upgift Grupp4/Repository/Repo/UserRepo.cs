81259f148e3bd7e757e8123bed61fdf4a0761726
Meenakshi1002 <meenakshi.amit.1001@gmail.com>
2024-01-12 13:33:37 +0100

    sdsdhsdh

--- Grupp upgift Grupp4/Repository/Repo/UserRepo.cs
+++ Grupp upgift Grupp4/Repository/Repo/UserRepo.cs
@@ -1,6 +1,33 @@
-﻿namespace Grupp_upgift_Grupp4.Repository.Repo
+﻿using Grupp_upgift_Grupp4.Models.Entities;
+using Grupp_upgift_Grupp4.Repository.Interface;
+using System.Data;
+
+namespace Grupp_upgift_Grupp4.Repository.Repo
 {
-    public class UserRepo
+    public class UserRepo :IUserRepo
     {
+        private readonly IDBContext _context;
+
+        public UserRepo(IDBContext context)
+        {
+            _context = context;
+        }
+
+        public List<User> GetAllUser()
+        {
+            try
+            {
+                using (IDbConnection db = _context.GetConnection())
+                {
+                    return db.Query<User>("ShowAllUsers", commandType: CommandType.StoredProcedure).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the exception details (replace Console.WriteLine with your logging mechanism)
+                Console.WriteLine($"An error occurred in GetAllUsers: {ex.Message}");
+                throw;
+            }
+        }
     }
 }
